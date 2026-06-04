ac74b2afedaa7a9e95d8e6efbdf20eadfc52c38c
ArpitUpadhyay1009 <arpitupadhyay186@gmail.com>
2026-06-04 12:15:12 +0530

    added header component

---
+++ components/header/header.tsx
@@ -0,0 +1,9 @@
+import "./header.css";
+
+export default function Header(){
+    return(
+        <header className="bg-purple-900 text-white p-8">
+            <h1 className="text-4xl font-bold text-center font-mono" >Report Design</h1>
+        </header>
+    )
+}
